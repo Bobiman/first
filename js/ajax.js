58b89c9e53711aad3ad62439e4c410895dab96ab
Boban Madzoski <boby_madzosky@hotmail.com>
2020-10-05 18:48:03 +0200

    last commit

--- js/ajax.js
+++ js/ajax.js
@@ -2,28 +2,45 @@
  * Basic call using XHR (legacy code)
  */
 
+// let button = document.getElementById("btn");
+
+// button.onclick = loadDoc();
+
 // function loadDoc() {
 //   var xhttp = new XMLHttpRequest();
 //   xhttp.onreadystatechange = function () {
 //     if (this.readyState == 4 && this.status == 200) {
-//       //document.getElementById("demo").innerHTML = this.responseText;
-//       console.log(this.responseText);
+//       //   document.getElementById("demo").innerHTML = this.responseText;
+//       let ul = document.getElementById("todos");
+
+//       for (let todo of JSON.parse(this.responseText)) {
+//         let li = document.createElement("li");
+
+//         li.innerHTML = todo.title;
+
+//         ul.appendChild(li);
+//       }
 //     }
 //   };
 //   xhttp.open("GET", "https://jsonplaceholder.typicode.com/todos/1", true);
 //   xhttp.send();
 // }
 
-// loadDoc();
-
 /**
  * Ajax with fetch API
  */
 
-// function loadDocWithFetch() {
-//   return fetch("https://jsonplaceholder.typicode.com/todos/1");
-// }
+function loadDocWithFetch() {
+  return fetch("https://jsonplaceholder.typicode.com/todos/1");
+}
 
-// let res = loadDocWithFetch();
+let result = loadDocWithFetch();
+console.log(result instanceof Promise);
 
-// res.then((res) => res.json()).then((data) => console.log(data));
+result
+  .then(function (result) {
+    result.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
